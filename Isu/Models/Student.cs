ec5cacce063ffa1e646dfb183f099a7723f32a21
pavel <pavelzavalnyuk11@gmail.com>
2021-09-14 21:09:36 +0300

    fix: IEquatable, default constructor

--- Isu/Models/Student.cs
+++ Isu/Models/Student.cs
@@ -1,10 +1,13 @@
+using System;
+
 namespace Isu.Models
 {
-    public class Student
+    public class Student : IEquatable<Student>
     {
         private readonly int _id;
         private readonly string _name;
 
+        public Student() { }
         public Student(string name)
         {
             _name = name;
@@ -13,5 +16,25 @@ namespace Isu.Models
 
         public int GetId() => _id;
         public string GetName() => _name;
+
+        public bool Equals(Student other)
+        {
+            if (other == null)
+                return false;
+            return _name == other._name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is not Student studentObj)
+                return false;
+            else
+                return Equals(studentObj);
+        }
+
+        public override int GetHashCode() => (_name, _id).GetHashCode();
     }
 }
